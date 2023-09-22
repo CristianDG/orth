45f8ba5eae3bd6cc096f5b6cc9b0ba2ef9db36b7
CristianDG <39601699+CristianDG@users.noreply.github.com>
2023-09-22 20:52:07 -0300

    feat: melhorando o parsing

--- orth.ml
+++ orth.ml
@@ -1,10 +1,10 @@
-open Core
+open Base
 
 type token =
   | Add
   | Sub
   | Mul
-  | Div
+  | IntDiv
   | PrintNumber
   | PrintChar
   | Swap
@@ -14,28 +14,55 @@ type token =
 
 type program = token list
 
-let lex (source : string) : token list =
+let rec lex (source : string) : token list =
   let chars : char list = String.to_list source in
   let rec loop ?(tokens : token list = []) = function
     | [] -> tokens
     | c :: rest ->
-        let token =
+        let res =
           match c with
-          | '+' -> Add
-          | v when Option.is_some (Int.of_string_opt (String.of_char v)) ->
-              let _s = String.of_char v in
-              Number 0
-          | _ -> assert false
+          | c when Char.is_whitespace c -> (None, rest)
+          | '+' -> (Some Add, rest)
+          | '/' -> (Some IntDiv, rest)
+          | '*' -> (Some Mul, rest)
+          | '-'
+            when match rest with
+                 | char :: _ -> Char.is_whitespace char
+                 | _ -> false ->
+              (Some Sub, rest)
+          | n
+            when equal_char '-' n
+                 || Option.is_some (Int.of_string_opt (String.of_char n)) ->
+              parse_int n rest
+          | c -> parse_word (c :: rest)
         in
-        loop ~tokens:(List.append tokens [ token ]) rest
 
+        loop
+          ~tokens:
+            (List.append tokens
+               (match fst res with Some token -> [ token ] | None -> []))
+          (snd res)
   in
   loop chars
 
-and take_while _f _chars = ""
+and parse_int n rest =
+  let s = String.of_char n in
+  let is_number n = Option.is_some (Int.of_string_opt (String.of_char n)) in
+  let number_str =
+    s ^ String.of_char_list (List.take_while ~f:is_number rest)
+  in
+  (Some (Number (Int.of_string number_str)), List.drop_while ~f:is_number rest)
+
+and parse_word source =
+  let str = List.take_while ~f:Char.is_alpha source |> String.of_char_list in
+  let consumed = List.drop_while ~f:Char.is_alpha source in
+  match str with
+  | "putn" -> (Some PrintNumber, consumed)
+  | "putc" -> (Some PrintChar, consumed)
+  | _ -> assert false
 
 let tokens_of_file (input_file_pahth : string) : token list =
-  lex (In_channel.read_all input_file_pahth)
+  lex (Core.In_channel.read_all input_file_pahth)
 
 let asm_of_token (token : token) : string =
   let instructions =
@@ -52,9 +79,10 @@ let asm_of_token (token : token) : string =
   List.map ~f:(fun s -> s ^ "\n") instructions |> String.concat
 
 let output_asm_to_file tokens output_file_path =
-  let open Out_channel in
+  let open Stdio.Out_channel in
   let ch = create output_file_path in
   (* inicialização *)
+  Core.printf "Inicializando\n";
   List.iter ~f:(output_string ch)
     [
       ".intel_syntax noprefix\n";
@@ -80,11 +108,17 @@ let output_asm_to_file tokens output_file_path =
       "    mov rbp, rsp\n";
     ];
 
+  printf "asm\n";
   List.iter ~f:(fun (t : token) -> asm_of_token t |> output_string ch) tokens;
 
   (* finalização *)
+  printf "finalizando\n";
   output_string ch "    pop rbp\n";
-  output_string ch "    ret\n"
+  output_string ch "    ret\n";
 
+  close ch
+
+(*
 let tokens = tokens_of_file "./examples/add.orth"
 let () = output_asm_to_file tokens "./tmp/teste.s"
+ *)
